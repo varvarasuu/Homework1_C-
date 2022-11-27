0a394b35d371fc0a40c07f302bf245c755b37975
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-11-27 16:53:38 +0300

    Правильно написала знак больше либо равно

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -3,7 +3,7 @@ Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1=>number2)
+if (number1>=number2)
     Console.WriteLine(number1); 
 else 
     Console.WriteLine(number2); 

890b8a74d9a0f5f5d40d1ce3612325977e39fa16
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-11-27 16:44:31 +0300

    Сделала задачу 2

---
+++ Задача2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1>number2)
+    Console.WriteLine(number1); 
+else 
+    Console.WriteLine(number2); 
+

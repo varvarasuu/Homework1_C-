e074dba2340f7fb6e2c083f8f5b988ff4c4a71d4
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-11-27 17:11:48 +0300

    Сделала задачу 6

---
+++ Задача6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n%2==0)
+    Console.Write("да, число четное");
+else 
+    Console.Write("нет, число нечетное");

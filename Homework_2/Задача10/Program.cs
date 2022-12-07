fce603f544ef454a845e81e867416ddc75c596e0
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-07 14:49:38 +0300

    Добавила папки для решения задач, сделала задачу 10

---
+++ Homework_2/Задача10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ((n/100>=10) || (n < 100) ) 
+    Console.WriteLine("Вы ввели не трехзначное число");
+else {
+    n = n/10;
+    n = n%10;
+    Console.WriteLine($"Вторая цифра числа {n}");
+}

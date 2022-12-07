f2f6e98b2922aee8bec43cf06b3588ed16761419
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-07 14:58:33 +0300

    Решила задачу 13

--- Задача6/Program.cs
+++ Задача6/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.Clear();
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-if (n%2==0)
-    Console.Write("да, число четное");
-else 
-    Console.Write("нет, число нечетное");
+
+int a = new Random().Next(100,1000);
+Console.WriteLine($"Сгенирированное число {a}");
+int a1 = a / 100;
+int a3 = a % 10;
+
+int b = a1*10 + a3;
+
+Console.Write($"Новое число {b}");

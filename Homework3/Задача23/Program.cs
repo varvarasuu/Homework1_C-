d4b7fe7b9d03860cb3ab6eb5bb163cfd9d02e201
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-07 17:03:09 +0300

    Решила задачу 23

---
+++ Homework3/Задача23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число N ");
+double N = Convert.ToDouble(Console.ReadLine());
+double i=1;
+while (i<=N)
+{
+    Console.Write($"{Math.Pow(i,3)} ");
+    i++;
+}

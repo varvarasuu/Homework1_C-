b7ee58bff088bd008e72b7a9549989166152ce61
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-07 14:59:59 +0300

    Решила задачу 13

---
+++ Homework_2/Задача13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<100) 
+    Console.WriteLine("Второй цифры числа нет");
+else {
+    while (n>=1000) {
+        n=n/10;
+    }
+    n = n%10;
+    Console.WriteLine($"Третья цифра числа {n}");
+}

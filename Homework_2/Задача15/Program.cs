8a82a2e2dd37a00c25b65aa9c44c649f8d9c0860
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-07 15:28:03 +0300

    Сделала задачу 15

---
+++ Homework_2/Задача15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ((n>7) || (n<=0)) 
+    Console.WriteLine("Такого дня недели нет");
+else if ((n==7) || (n==6)) 
+    Console.WriteLine("да");
+else 
+    Console.WriteLine("нет");

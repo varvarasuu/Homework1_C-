c2dcec458de542fe50e723ed0e321487919a6f8e
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-13 17:51:29 +0300

    Решила задачу 25 и создала папки для решения задач

---
+++ Homework4/Задача25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int n =1;
+for (int i=0; i<b; i++){
+    n = n*a;
+}
+Console.Write($"A в степени B равно: {n}");

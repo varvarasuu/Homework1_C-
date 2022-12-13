9cbfdc72f157112ad34c74ab7ea70dc5a2a88620
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-13 18:00:42 +0300

    Решила задачу 27

---
+++ Homework4/задача27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m;
+int sum=0;
+while (n>0){
+    m=n%10;
+    sum+=m;
+    n/=10;
+}
+Console.Write($"Сумма цифр в числе равна: {sum}");

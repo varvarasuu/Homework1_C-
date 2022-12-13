41fabbaeb9b353a1865a0d43bcbf2accf29280d6
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-13 18:10:51 +0300

    Решила задачу 29

---
+++ Homework4/Задача29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+for (int i=0; i<n; i++) {
+    Console.Write($"Введите {i} элемент массива: ");
+    array [i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");

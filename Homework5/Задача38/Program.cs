b2b472754c6846552022b5ed23afc333e38dd5d2
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-16 14:19:46 +0300

    Сделала задачу 38

---
+++ Homework5/Задача38/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double [] array = new double [n];
+for (int i=0; i< array.Length; i++) {
+    array [i] = new Random().Next(-100,101);
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+double max =0;
+double min = 101;
+for (int i=0; i< array.Length; i++) {
+    if (array[i]>max) 
+    max = array[i];
+}
+for (int i=0; i< array.Length; i++) {
+    if (array[i]<min) 
+    min = array[i];
+}
+
+Console.Write(max-min);

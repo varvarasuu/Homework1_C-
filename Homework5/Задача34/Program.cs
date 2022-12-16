d2472330d40e384d29b96c7abf6fa1a82b03e31c
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-16 14:05:47 +0300

    Добавила папки для решения задач и решила задачу 34

---
+++ Homework5/Задача34/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+for (int i=0; i< array.Length; i++) {
+    array [i] = new Random().Next(100,1000);
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int count=0;
+for (int i=0; i<array.Length; i++) {
+    if (array[i]%2==0)
+    count++;
+}
+Console.WriteLine(count);

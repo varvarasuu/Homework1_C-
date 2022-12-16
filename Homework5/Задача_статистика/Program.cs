c7feaa524124dc3f40b8e7f4921822b72d99f422
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-16 16:37:29 +0300

    Сделала задачу статистика

---
+++ Homework5/Задача_статистика/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+for (int i=0; i< array.Length; i++) {
+    array [i] = new Random().Next(1,32);
+}
+int [] array4 = new int [n];
+int [] array3 = new int [n];
+int count4 =0 ;
+for (int i=0; i< array.Length; i++) {
+    if (array[i]%2==0) {
+    array4[i] = array[i];
+    count4++;
+    }
+    else 
+    array3[i] = array[i];
+}
+for (int i=0; i< array.Length; i++) {
+    if (array3[i]!=0)
+    Console.Write($"{array3[i]} ");
+}
+Console.WriteLine("");
+for (int i=0; i< array.Length; i++) {
+    if (array4[i]!=0)
+    Console.Write($"{array4[i]} ");
+}
+Console.WriteLine("");
+if (count4>=n-count4) 
+Console.Write("YES");
+else 
+Console.Write("NO");

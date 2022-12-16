22298ac36745aa4c36f0e868729073e65638719f
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-16 14:11:55 +0300

    Сделала задачу 36

---
+++ Homework5/Задача36/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+for (int i=0; i< array.Length; i++) {
+    array [i] = new Random().Next(-100,101);
+}
+int summa =0;
+Console.WriteLine($"[{string.Join(", ", array)}]");
+for (int i=0; i< array.Length; i++) {
+    if (i%2==1) {
+        summa += array[i];
+    }
+}
+Console.Write(summa);

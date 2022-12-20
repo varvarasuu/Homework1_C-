6df19bf1254252f52616ee163236ca52ba29888e
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-20 12:27:58 +0300

    Добавила папки для решения задач и решила задачу 41

---
+++ Homework6/Задача41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите последовательность чисел через пробел: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int CountM (int []array) {
+    int count =0;
+    foreach(int element in array) {
+        if (element >0 )
+        count++;
+    }
+    return count;
+}
+
+Console.Write(CountM(array));

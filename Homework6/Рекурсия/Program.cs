ad8c440f43c9e6e1bd38266e6e3a486b8e22cd07
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-20 13:47:58 +0300

    Попыталась решить задачу Рекурсия

---
+++ Homework6/Рекурсия/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите строку: ");
+string N = Console.ReadLine();
+void perestanovka (string N) {
+    char k = N[0];
+    for (int i=0; i<N.Length-1; i++) {
+        N[i]=N[i+1];
+    }
+    N[N.Length-1]=k;
+    Console.WriteLine(N);
+    perestanovka(N);
+}
+perestanovka(N);

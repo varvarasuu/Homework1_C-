f335823d5f57c1e11423030a5ebb4f849a9b379f
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-23 14:48:23 +0300

    Добавила папки и сдалала задачу 47

---
+++ Homework7/Задача47/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк массива m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double [m,n];
+for (int i=0; i<array.GetLength(0); i++) {
+    for (int j=0; j<array.GetLength(1); j++) {
+        array[i,j] = new Random().NextDouble() * (10 + 10) - 10;
+        array[i,j] = Math.Round(array[i,j],2);
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+

0708f1ab7e2e1193187fbd784eef016f8ba9eeec
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-23 15:02:21 +0300

    Решила задачу 50

---
+++ Homework7/Задача50/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Введите количество строк массива m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double [m,n];
+for (int i=0; i<array.GetLength(0); i++) {
+    for (int j=0; j<array.GetLength(1); j++) {
+        array[i,j] = new Random().NextDouble() * (10 + 10) - 10;
+        array[i,j] = Math.Round(array[i,j],2);
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите количество строку массива i: ");
+int i1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбец массива j: ");
+int j1 = Convert.ToInt32(Console.ReadLine());
+if (i1<array.GetLength(0) && i1>=0) {
+if (j1<array.GetLength(1) && j1>=0) 
+    Console.Write(array[i1,j1]);
+else 
+    Console.Write("такой позиции в массиве нет");
+}
+else 
+    Console.Write("такой позиции в массиве нет");

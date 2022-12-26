b69adb289b928daf35705768b5d32989a5b3ea4a
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-26 22:11:27 +0300

    Попыталась решить задачи 61 и 62

---
+++ Homework8/Задача61/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [N,N+N-1];
+void MatrixSet (int[,] array) {
+
+ for (int i=0; i<array.GetLength(0); i++) 
+{   for (int j=0; j<array.GetLength(0)-3;j++){
+    array[i,(N+N-2)/2+i]=1;
+    array[i,(N+N-2)/2-i]=1;
+    if (i>1){
+    array[i,((N+N-2)/2+i-2)]=array[i-1,(N+N-2)/2+i-1]+array[i-1,(N+N-2)/2-i+1];
+    array[i,((N+N-2)/2-i+2)]=array[i-1,(N+N-2)/2+i-1]+array[i-1,(N+N-2)/2-i+1];
+    if (i>3)
+    array[i,((N+N-2)/2+i-4)]=6;
+    }
+}
+}
+}
+void MatrixPrint (int[,] array) {
+for (int i=0; i<array.GetLength(0); i++) {
+    for (int j=0; j<array.GetLength(1); j++) {
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+}
+MatrixSet(array);
+MatrixPrint(array);
---
+++ Homework8/Задача62/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int [,] array = new int [4,4];
+void MatrixPrint (int[,] array) {
+for (int i=0; i<array.GetLength(0); i++) {
+    for (int j=0; j<array.GetLength(1); j++) {
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+}
+int i=0, j=0;
+while (i!=3 && j!=2){
+    int k=0, count=0;
+    while (j<array.GetLength(1)-k) {
+    array[i,j]=count;
+    count++;
+    j++;
+    if (j==array.GetLength(1)-k-1){
+        i=j;
+        j=0;
+        k++;
+    }
+    }
+    
+}
+    
+

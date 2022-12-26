bcd62b8ad87fd081c7271c9b76e0118c9ab5a2a5
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-26 13:02:26 +0300

    Решила задачу 56

---
+++ Homework8/Задача56/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк массива m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [m,n];
+void MatrixSet (int[,] array) {
+for (int i=0; i<array.GetLength(0); i++) {
+    for (int j=0; j<array.GetLength(1); j++) {
+        array[i,j] = new Random().Next(0, 11);
+    }
+}
+}
+void MatrixPrint (int[,] array) {
+for (int i=0; i<array.GetLength(0); i++) {
+    for (int j=0; j<array.GetLength(1); j++) {
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+}
+int MatrixMin (int[,] array) {
+int [] stroki = new int [array.GetLength(0)];
+for (int i=0; i<array.GetLength(0); i++) {
+    for (int j=0; j<array.GetLength(1); j++) {
+        stroki[i]+=array[i,j];
+    }
+}
+    int min =0;
+    for (int i=0; i<stroki.Length; i++) {
+        if (stroki[i]<stroki[min]) 
+        min=i;
+    }
+    return min;
+}
+MatrixSet(array);
+MatrixPrint(array);
+Console.WriteLine($"{MatrixMin(array)+1} строка");

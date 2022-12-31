c531f1f7914b6ca95a7e043ed8b0669c690c40da
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2022-12-31 20:25:30 +0300

    Решила задачу 64

---
+++ Homework9/Задача64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите M: ");
+int m=int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int n=int.Parse(Console.ReadLine());
+Console.WriteLine(PrintNumbers(n,m));
+ string PrintNumbers(int end, int start)
+{
+    if (end == start)
+    {
+        // Console.WriteLine(m);
+        return end.ToString();
+    }
+    string s = PrintNumbers(end, start+1) + ' ' + start.ToString();
+    Console.WriteLine(s);
+    return (s);
+}

cc617727135451b3c2c387144b891267b7ccd3a4
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2023-01-01 14:55:55 +0300

    Сделала задачу 68

---
+++ Homework9/Задача68/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите n: ");
+int n=int.Parse(Console.ReadLine());
+Console.Write("Введите m: ");
+int m=int.Parse(Console.ReadLine());
+Console.WriteLine(ack(n,m));
+int ack(int n, int m)
+{
+   if (n == 0)
+     return m + 1;
+   else if (m == 0)
+     return ack (n - 1, 1);
+   else
+     return ack(n - 1, ack (n, m - 1));
+}

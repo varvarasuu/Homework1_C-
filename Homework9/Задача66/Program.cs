a150efb45a9b63492a76caf0be7030050408fe00
Varvara Sumenkova <sweet.sad.lydia@gmail.com>
2023-01-01 14:39:47 +0300

    Сделала задачу 66

---
+++ Homework9/Задача66/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите M: ");
+int m=int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int n=int.Parse(Console.ReadLine());
+Console.WriteLine(SumNumbers(m,n));
+int SumNumbers(int a, int b)
+{
+    if (a == b+1)
+    {
+        return 0;
+    }
+    a += SumNumbers(a+1, b);
+    return (a);
+}
